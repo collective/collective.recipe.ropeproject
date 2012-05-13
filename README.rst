3d5fcfe3ddd40456ba53022bc784f56d3442928f
marciomazza <marciomazza@gmail.com>
2012-05-13 17:52:38 -0300

    better docs and a more reasonable version number: 0.2

--- README.rst
+++ README.rst
@@ -1,17 +1,16 @@
-(TODO: make a better README)
+Usage
+=====
 
-Usage:
+In your buildout.cfg include the recipe, with the eggs property set to whatever you need
+(tipically ${instance:eggs})::
 
-In your buildout.cfg include the recipe, with the eggs property set to whatever you need (e.g. ${instance:eggs}):
-
-
-[buildout]
-...
-parts = ... ropeproject
-
-[instance]
-eggs = ...
-
-[ropeproject]
-recipe = collective.recipe.ropeproject
-eggs = ${instance:eggs}
+    [buildout]
+    ...
+    parts = ... ropeproject
+    
+    [instance]
+    eggs = ...
+    
+    [ropeproject]
+    recipe = il.recipe.ropeproject
+    eggs = ${instance:eggs}
--- setup.py
+++ setup.py
@@ -1,20 +1,41 @@
 from setuptools import setup, find_packages
+import os
 
 name = "collective.recipe.ropeproject"
-version = '1.0b1'
+version = '0.2'
 
-# TODO: remove defaults
+read = lambda f: open(f).read()
+
+long_description = (
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('README.rst')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' + 
+    read('CHANGES.txt')
+    # + '\n' +
+    # 'Contributors\n' 
+    # '************\n'
+    # + '\n' +
+    # read('CONTRIBUTORS.txt')
+    )
+
 setup(
     name = name,
     version = version,
     author = "Marcio Mazza",
     author_email = "marciomazza@gmail.com",
     description = "zc.buildout recipe for creating a .ropeproject with configured paths for eggs and dependencies",
+    long_description = long_description,
     packages = find_packages('.'),
     include_package_data = True,
     package_dir = {'':'.'},
     namespace_packages = ['collective', 'collective.recipe'],
     install_requires = [
+        'setuptools',
         'zc.recipe.egg',
         'rope'
     ],
